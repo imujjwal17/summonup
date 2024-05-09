c944e3bdc45ef553a06c35cc585d70ec88a9b0ea
Kunwar Ujjwal Singh <kujjwal21378@gmail.com>
2024-05-09 18:30:41 +0530

    authentication feature added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ClerkProvider
+      appearance={{
+        layout:{
+          logoImageUrl:'/icons/logo.svg',
+          socialButtonsVariant:'iconButton'
+        },
+        variables:{
+          colorBackground:'#1c1f2e',
+          colorText:'#fff',
+          colorInputText:'#fff',
+          colorPrimary:'#0E78F9',
+          colorInputBackground:'#252a41'
+        }
+      }
+      }
+      >
       <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
