223ebfc607942af05e0d2f00bc8a503c0a0fd035
Kunwar Ujjwal Singh <kujjwal21378@gmail.com>
2024-05-25 20:13:02 +0530

    meeting modal created

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,16 +4,20 @@ import Image from "next/image";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import MeetingModal from "./MeetingModal";
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined>();
+  const createMeeting =()=>{
+    
+  }
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -36,6 +40,15 @@ const MeetingTypeList = () => {
         description="via invitation link"
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
+      />
+
+      <MeetingModal 
+        isOpen={meetingState==='isInstantMeeting'}
+        onClose={()=>setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="StartMeeting"
+        handleClick={createMeeting}
       />
     </section>
   );
