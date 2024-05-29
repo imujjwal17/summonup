834b178bd4ae36ae18d0507cbbb5d927cea16ad4
Kunwar Ujjwal Singh <kujjwal21378@gmail.com>
2024-05-29 20:17:07 +0530

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,8 +1,10 @@
-import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { cn } from '@/lib/utils'
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 const MeetingRoom = () => {
   const [layout , setLayout] = useState<CallLayoutType>('speaker-left')
+  const [showParticipants , setShowParticipants] = useState(false)
   const CallLayout =()=> {
     switch (layout) {
       case 'grid':
@@ -21,6 +23,15 @@ const MeetingRoom = () => {
         <div className="flex size-full max-w-[1000px] items-center">
           <CallLayout />
         </div>
+        <div className={cn('h-[calc(100vh-86px)] hidden ml-2')} >
+          <CallParticipantsList 
+          onClose={()=>{
+            setShowParticipants(false)}}
+          />
+        </div>
+      </div>
+      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+            <CallControls />
       </div>
     </section>
   )
